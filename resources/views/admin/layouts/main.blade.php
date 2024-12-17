20caa109545125fc31e290ebbf364cdc54380690
abizar14 <abizarzauli24@gmail.com>
2024-12-17 20:39:04 +0800

    feat : layouts blade

---
+++ resources/views/admin/layouts/main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Dashboard - Voler Admin Dashboard')</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <link rel="stylesheet" href="assets/vendors/chartjs/Chart.min.css">
+
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+</head>
+
+<body>
+    <div id="app">
+        @include('admin.layouts.sidebar')
+        <div id="main">
+            @include('admin.layouts.navbar')
+
+            <div class="main-content container-fluid">
+                @yield('content')
+            </div>
+
+            @include('admin.layouts.footer')
+</body>
+
+</html>
