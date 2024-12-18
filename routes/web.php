3cb9c83a7ce5ddd61a9ce04e6d3a2f931c82a9c4
abizar14 <abizarzauli24@gmail.com>
2024-12-18 18:36:37 +0800

    fixed : route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PegawaiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.dashboard.index');
 });
+
+Route::prefix('pegawai')->group(function() {
+    Route::get('/', [PegawaiController::class, 'index'])->name('pegawai.index');
+    Route::get('/show/{id}', [PegawaiController::class, 'show'])->name('pegawai.show');
+    Route::post('/', [PegawaiController::class, 'store'])->name('pegawai.store');
+    Route::get('/edit/{id}', [PegawaiController::class, 'edit'])->name('pegawai.edit');
+    Route::put('/update/{id}', [PegawaiController::class, 'update'])->name('pegawai.update');
+    Route::delete('/{id}', [PegawaiController::class, 'destroy'])->name('pegawai.destroy');
+});
+
+// Route::middleware(['auth', 'role:pegawai'])->group(function () {
+//     Route::resource('pegawai', PegawaiController::class);
+// });
+
+// Route::middleware(['auth', 'role:admin'])->group(function () {
+
+// });
