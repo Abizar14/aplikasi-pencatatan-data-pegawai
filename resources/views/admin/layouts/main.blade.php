d58ed2f2531b7cffef68459783688c6f77cde38d
abizar14 <abizarzauli24@gmail.com>
2024-12-18 18:37:03 +0800

    update : layouts view

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -2,16 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Dashboard - Voler Admin Dashboard')</title>
 
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
 
-    <link rel="stylesheet" href="assets/vendors/chartjs/Chart.min.css">
+    <link rel="stylesheet" href="{{ asset("assets/css/bootstrap.css") }}">
 
-    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
-    <link rel="stylesheet" href="assets/css/app.css">
-    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset("assets/vendors/chartjs/Chart.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("assets/vendors/simple-datatables/style.css") }}">
+    <link rel="stylesheet" href="{{ asset("assets/vendors/perfect-scrollbar/perfect-scrollbar.css") }}">
+    <link rel="stylesheet" href="{{ asset("assets/css/app.css") }}">
+    <link rel="shortcut icon" href="{{ asset("assets/images/favicon.svg") }}" type="image/x-icon">
 </head>
 
 <body>
